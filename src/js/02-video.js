f13415f59037e4a0a8601152529d247f7375bc57
Dmytro <savchyndd@gmail.com>
2022-10-22 12:08:25 +0300

    add complited task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+// Import library
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+// Constant value entry
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+// Get videoplayer element and a player
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// Set and get current time from localStorage
+const onPlay = ({ seconds }) =>
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, seconds);
+const videoplayerCurrentTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
+
+// Start traking the timeupdate event with a delay 1s and set current time
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(videoplayerCurrentTime);
